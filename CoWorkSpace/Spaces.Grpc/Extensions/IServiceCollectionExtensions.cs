260bb87a5d67edb0557bfe5778e40e0b6fe11fbd
Darko Micanovic <micanovic.d@gmail.com>
2023-08-06 00:55:06 +0200

    #1 Dodat Spaces.Grpc

---
+++ CoWorkSpace/Spaces.Grpc/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Spaces.Common.Interfaces;
+using Spaces.Common.Services;
+using Spaces.Persistance.Context;
+using Spaces.Persistance.Helpers;
+using Spaces.Persistance.Interfaces;
+using Spaces.Persistance.Repositories;
+
+namespace Spaces.Grpc.Extensions
+{
+    internal static class IServiceCollectionExtensions
+    {
+        internal static void AddCommonServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<SpacesDbSettings>(configuration.GetSection("SpacesDbSettings"));
+            services.AddTransient<ISpaceContext, SpaceContext>();
+            services.AddTransient<ISpaceRepository, SpaceRepository>();
+            services.AddTransient<ISpaceService, SpaceService>();
+        }
+    }
+}
