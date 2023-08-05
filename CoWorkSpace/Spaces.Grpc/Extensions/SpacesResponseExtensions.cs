2bc28ed8b00c264b406d20efe377b719e2009a2d
eGoooD <37847565+DarkoMicanovic@users.noreply.github.com>
2023-08-06 00:59:03 +0200

    #1 Dodat Spaces.Grpc (#5)

---
+++ CoWorkSpace/Spaces.Grpc/Extensions/SpacesResponseExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Spaces.Grpc.Protos;
+
+using SpaceModel = Spaces.Common.Models.Space;
+
+namespace Spaces.Grpc.Extensions
+{
+    internal static class SpacesResponseExtensions
+    {
+        internal static Space ToGrpsModel(this SpaceModel model)
+        {
+            return new Space
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Address = model.Address
+            };
+        }
+
+        internal static IEnumerable<Space> ToGrpsModel(this IEnumerable<SpaceModel> models)
+        {
+            return models.Select(model => model.ToGrpsModel());
+        }
+    }
+}
---
+++ CoWorkSpace/Spaces.Grpc/Services/SpaceGrpcService.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Core;
+using Spaces.Common.Interfaces;
+using Spaces.Grpc.Extensions;
+using Spaces.Grpc.Protos;
+
+using SpaceModel = Spaces.Common.Models.Space;
+
+namespace Spaces.Grpc.Services
+{
+    public sealed class SpaceGrpcService : SpaceProtoService.SpaceProtoServiceBase
+    {
+        private readonly ISpaceService spaceService;
+
+        public SpaceGrpcService(ISpaceService spaceService)
+        {
+            this.spaceService = spaceService;
+        }
+
+        #region SpaceProtoServiceBase Overrides
+
+        public override async Task<GetSpacesResponse> GetSpaces(GetSpacesRequest request, ServerCallContext context)
+        {
+            IEnumerable<SpaceModel> spaceModels = await this.spaceService.GetAllAsync();
+
+            var response = new GetSpacesResponse();
+            response.Spaces.AddRange(spaceModels.ToGrpsModel());
+
+            return response;
+        }
+
+        #endregion
+    }
+}
