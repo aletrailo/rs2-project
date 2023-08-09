3c0b8756ea42e66969121cfcd7a115169c64bbf9
aletrailo <aleksandratrailovic97@gmail.com>
2023-08-09 22:12:40 +0100

    Identity Server - roles and users #9

---
+++ CoWorkSpace/IdentityServer.Api/Extensions/IdentityExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using IdentityServer.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using IdentityServer.Api.Enitities;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace IdentityServer.Api.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static IServiceCollection ConfigurePersistence(this IServiceCollection servies, IConfiguration configuration) {
+            servies.AddDbContext<ApplicationContext>(options => {
+                options.UseSqlServer(configuration.GetConnectionString("IdentityConnectionString"));
+            });
+
+            return servies;
+        }
+
+        public static IServiceCollection ConfigureIdentity(this IServiceCollection services) {
+            services.AddIdentity<User, IdentityRole>(options => {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 8;
+                options.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<ApplicationContext>().AddDefaultTokenProviders();
+
+            return services;
+         }
+    }
+}
