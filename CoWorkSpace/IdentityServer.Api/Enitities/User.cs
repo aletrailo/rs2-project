04877e4625974739b09ec97a974f2e8e0c7f594e
aletrailo <aleksandratrailovic97@gmail.com>
2023-08-14 17:27:23 +0100

    Povezan SQL Server i kreirane migracije

--- CoWorkSpace/IdentityServer.Api/Enitities/User.cs
+++ CoWorkSpace/IdentityServer.Api/Enitities/User.cs
@@ -5,9 +5,14 @@ namespace IdentityServer.Api.Enitities
     public class User : IdentityUser
     {
         public string FirstName { get; set;}
-        public string LastNamev { get; set; }
 
+        public string LastName { get; set;}
 
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
 
     }
 }
--- CoWorkSpace/IdentityServer.Api/Mapper/IdentityProfile.cs
+++ CoWorkSpace/IdentityServer.Api/Mapper/IdentityProfile.cs
@@ -7,9 +7,11 @@ namespace IdentityServer.Api.Mapper
     public class IdentityProfile : Profile
     {
         public IdentityProfile() {
-            CreateMap<User, NewUserDto>().ReverseMap();
+            CreateMap<User, NewUserDto>().ReverseMap()
+                .ForMember(x => x.UserName, y => y.MapFrom(user => user.UserNeme))
+                .ForMember(x => x.PasswordHash, y => y.MapFrom(user => user.Password));
             CreateMap<User, UserDetails>().ReverseMap();
-        
+            
         }
     }
 }
