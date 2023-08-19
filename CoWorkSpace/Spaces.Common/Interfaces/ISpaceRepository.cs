42520e4e92b5ebd8313f50b46cee6b1493c0811e
Milica Kleut <121762858+kleutMilica@users.noreply.github.com>
2023-08-19 16:38:07 +0200

    Implementirani Insert, Update, Delete i GetById metodi u Spaces#20

--- CoWorkSpace/Spaces.Common/Interfaces/ISpaceRepository.cs
+++ CoWorkSpace/Spaces.Common/Interfaces/ISpaceRepository.cs
@@ -7,5 +7,9 @@ namespace Spaces.Common.Interfaces
     public interface ISpaceRepository
     {
         Task<IEnumerable<Space>> GetAllSpaces();
+        Task InsertSpace(Space space);
+        Task<bool> UpdateSpace(Space space); 
+        Task<bool> DeleteSpace(string Id);
+        Task<Space> GetSpaceById(string Id);
     }
 }
