5ee46d472460fafd4f690c7685fa0cacf8b1ae9f
eGoooD <37847565+DarkoMicanovic@users.noreply.github.com>
2023-08-20 20:36:48 +0200

    Development docker fix (#29)
    * Popravljena konekcija za mssql preko Docker-a
    
    * Usaglasene verzije Microsoft.VisualStudio.Azure.Containers.Tools.Targets paketa

---
+++ CoWorkSpace/IdentityServer.Api/Extensions/HostExtensions.cs
@@ -0,0 +1,41 @@
+﻿using IdentityServer.Api.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Polly;
+
+namespace IdentityServer.Api.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context = services.GetService<ApplicationContext>();
+
+                try
+                {
+                    var retry = Policy.Handle<SqlException>()
+                        .WaitAndRetry(
+                            retryCount: 5,
+                            sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+
+                    retry.Execute(() => MigrateDatabase(context));
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+
+            return host;
+        }
+
+        private static void MigrateDatabase(ApplicationContext context)
+        {
+            context.Database.Migrate();
+        }
+    }
+}
