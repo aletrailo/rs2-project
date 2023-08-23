2c9a9aa55f58959dff3e9cf44eb7234e835ef91e
aletrailo <92488113+aletrailo@users.noreply.github.com>
2023-08-24 00:37:36 +0200

    Development identiy3 (#36)
    * Cors
     *Client- LogIn and SingIng page
    * Save access and refresh tokens when user refresh page or close and again open browser so we do not need to login again if they are valid
    *Code refactor based on vuex-typescript model. #35

--- CoWorkSpace/IdentityServer.Api/Program.cs
+++ CoWorkSpace/IdentityServer.Api/Program.cs
@@ -1,4 +1,7 @@
 using IdentityServer.Api.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 
 builder.Services.ConfigurePersistence(builder.Configuration);
@@ -14,15 +16,25 @@ builder.Services.ConfigureIdentity();
 builder.Services.ConfigureJWT(builder.Configuration);
 builder.Services.ConfigureMiscellaneousService();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "IdentityServer", Version = "v1" });
+});
+
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "IdentityServer v1"));
 }
-
+app.UseCors("CorsPolicy");
+app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
