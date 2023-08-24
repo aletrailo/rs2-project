7d163e9254c40846059c3a6bc56052cc05b9db91
Milica Kleut <121762858+kleutMilica@users.noreply.github.com>
2023-08-24 16:43:07 +0200

    CSS

--- CoWorkClient/cowork-app/src/views/LogIn.vue
+++ CoWorkClient/cowork-app/src/views/LogIn.vue
@@ -1,28 +1,42 @@
 <template>
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">LogIn strana</div>
-                <div class="card-body">
-                    <form @submit.prevent="login">
-                        <div class="mb-3">
-                            <label for="username" class="form-label">Username</label>
-                            <input v-model="username" type="text" id="username" class="form-control" required />
+    <div class="background-container">
+        <div class="container mt-5">
+            <div class="title-container">
+                <h1 class="title">
+                    Tražite poslovni prostor? Na pravom ste mestu.
+                </h1>
+                <p class="subtitle">
+                    Ulogujte se i pronađite savrsen prostor za Vase potrebe!
+                </p>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header">LogIn strana</div>
+                        <div class="card-body">
+                            <form @submit.prevent="login">
+                                <div class="mb-3">
+                                    <label for="username" class="form-label"></label>
+                                    <input v-model="username" type="text" id="username" placeholder="Username"
+                                        class="form-control" required />
+                                </div>
+                                <div class="mb-3">
+                                    <label for="password" class="form-label"></label>
+                                    <input v-model="password" type="password" id="password" placeholder="Password"
+                                        class="form-control" required />
+                                </div>
+                                <button type="submit" class="btn btn-primary">Login</button>
+                            </form>
+                            Nemate nalog?
+                            <button class="btn btn-default"><router-link :to="{ name: 'SingUp' }"
+                                    class="text-warning">KREIRAJ
+                                    NALOG</router-link></button>
                         </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
-                            <input v-model="password" type="password" id="password" class="form-control" required />
-                        </div>
-                        <button type="submit" class="btn btn-primary">Login</button>
-                    </form>
-                    nemate nalog?
-                    <button class="btn btn-default"><router-link :to="{ name: 'SingUp'}">KREIRAJ NALOG</router-link></button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </template>
 
 <script>
@@ -44,12 +58,59 @@ export default {
             })
         }
     },
-    computed:{
+    computed: {
     }
 
 }
 </script>
 
 <style scoped>
+.background-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url('@/assets/novo.png');
+    background-size: 100% 100%;
+    z-index: -1;
+}
 
-</style>
+.card {
+    background-image: url('@/assets//image.png');
+    background-size: 160%;
+    background-position: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) scale(1);
+    text-align: center;
+}
+
+.username,
+.password {
+    display: block;
+    margin-bottom: 10px;
+    padding: 5px;
+}
+
+.btn.btn-primary {
+    background-color: #f4d706;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+    /*border-radius: 30px; */
+    width: 130px;
+    height: 45px;
+}
+
+.text-warning {
+    color: #f4d706;
+}
+
+
+.card-body {
+    padding: 60px;
+    margin-top: 150px;
+    transform: scale(0.75);
+}</style>
