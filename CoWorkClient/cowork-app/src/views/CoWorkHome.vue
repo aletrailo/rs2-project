8da6a675f804001eb27b7cf8d88355117cfa3bbb
aletrailo <aleksandratrailovic97@gmail.com>
2023-08-29 14:10:30 +0200

    Client: CSS main view

--- CoWorkClient/cowork-app/src/views/CoWorkHome.vue
+++ CoWorkClient/cowork-app/src/views/CoWorkHome.vue
@@ -1,20 +1,39 @@
 <template>
 <MenuOptions />
-<div>POCETNA STRANICA</div>
-WELCOME {{user.firstName}}
 
-<router-link :to="{name: 'SpaceAdvertisement'}"><button type="button">Dodajte oglas</button></router-link>
-SVi oglasi
-<div v-for="ad in advertisements" :key="ad" style="border: 1px solid black; border-radius: 4px; margin-top:10px">
+<section class="text-center" style="background-color: #f4f4f4;">
+    <div class="row">
+        <div class="col-lg-6 col-md-8 mx-auto py-2">
+            <h1 class="fw-light"> <span style="color: yellow;">Co</span>working Space</h1>
+            <p class="lead text-muted">Make work more enjoyable</p>
+            <p>
+                <router-link :to="{ name: 'SpaceAdvertisement' }"><button type="button" class="btn btn-outline-secondary">Posedujete prostor? Oglasite ga!</button></router-link>
+            </p>
+        </div>
+    </div>
+</section>
+<div class="container">
 
-        naziv: {{ad.name}}<br>
-        adresa: {{ad.address}}<br>
-        opis: {{ad.description}}<br>
-        slobodan: {{ad.isFree}}<br>
-        cenaposatu: {{ad.pricePerHour}}<br>
-        slika: <img :src="ad.image" v-if="ad.image" style="max-width: 300px;" />
+    <div class="row row-cols-1 row-cols-md-4 g-8 py-4">
+        <div class="col" v-for="ad in advertisements" :key="ad">
+            <div class="card" style="height: 100%;">
+                <img src="../assets/novo.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title fw-bolder">{{ ad.name }}</h5>
+                    <div class="card-text">
+                        <div class="fw-bold"> <i class="bi bi-geo-alt-fill"></i> {{ ad.address }}</div>
+                        <div> {{ ad.description }}</div>
+                        <div> Trenutno: {{ ad.isFree? 'slobodan' : 'zauzet' }}</div>
+                        <div style="color: gray;"> <i class="bi bi-cash"></i> {{ ad.pricePerHour }} po satu</div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <button type="button" class="btn btn-outline-secondary">pogledajte detalje</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-
 </template>
 
 <script>
@@ -35,7 +54,7 @@ export default {
         user() {
             return this.$store.state.auth.user
         },
-        advertisements(){
+        advertisements() {
             return this.$store.state.advertisement.advertisements
         }
     }
