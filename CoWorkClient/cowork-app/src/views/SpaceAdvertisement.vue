4746bc285445924e502b9faab28a3ed367c6e593
aletrailo <aleksandratrailovic97@gmail.com>
2023-09-01 20:22:15 +0200

    Obavezno popunjavanje polja prilikom oglasavanja

--- CoWorkClient/cowork-app/src/views/SpaceAdvertisement.vue
+++ CoWorkClient/cowork-app/src/views/SpaceAdvertisement.vue
@@ -10,41 +10,41 @@
 <div class="container">
     <div class="row justify-content-md-center">
         <div class="col col-md-8 py-2">
-            <div class=" row" style="height: 100%;">
+            <form class="row" @submit.prevent="addAd" style="height: 100%;">
                 <div class="col-sm-6">
                     <div v-if="this.previewImage"  class="mb-2"  style="width: 300px;">
                         <img :src="this.previewImage" style="max-width: 300px;"/>
                     </div>
                     <div v-else class="mb-2" style="width: 300px; height: 300px; background-color: gray;">
                     </div>
-                    <input type="file" @change="handleFileChange" />
+                    <input type="file" @change="handleFileChange" required/>
                 </div>
                 <div class="col-sm-6">
                     <h5 class="card-title fw-bolder mb-2">
                         <label for="naziv" class="form-label">Naziv prostora</label>
-                        <input type="text" v-model="ad.name" class="form-control" id="naziv">
+                        <input type="text" v-model="ad.name" class="form-control" id="naziv" required/>
                     </h5>
                     <div class="card-text">
                         <div class="fw-bold mb-2"> <i class="bi bi-geo-alt-fill"></i>
                             <label for="address" class="form-label">Adresa</label>
-                            <input type="text" v-model="ad.address" class="form-control" id="address">
+                            <input type="text" v-model="ad.address" class="form-control" id="address" required/>
                         </div>
                         <div class="fw-bold mb-2">
                             <label for="inputAddress2" class="form-label">Opis</label>
-                            <input type="text" v-model="ad.description" class="form-control" id="inputAddress2">
+                            <input type="text" v-model="ad.description" class="form-control" id="inputAddress2" required/>
                         </div>
                         <div class="fw-bold mb-2"> <i class="bi bi-cash"></i>
                             <label for="price" class="form-label">Cena</label>
                             <div style="display: flex;">
-                                <input type="text" v-model="ad.pricePerHour" class="form-control" id="price" style="width: 200px;">
+                                <input type="text" v-model="ad.pricePerHour" class="form-control" id="price" style="width: 200px;" required/>
                                 <div style=" margin-left: 5px;">rsd po satu</div>
                             </div>
                         </div>
-                        <div> <button @click="addAd" type="button" class="btn btn-primary" style="float: right;">Dodaj</button></div>
+                        <div> <button type="submit" class="btn btn-primary" style="float: right;">Dodaj</button></div>
                     </div>
                 </div>
 
-            </div>
+            </form>
         </div>
     </div>
 </div>
