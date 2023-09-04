c47b095e5fc28aa55e3e6896843b0646d5a5d7bd
aletrailo <aleksandratrailovic97@gmail.com>
2023-09-05 01:18:33 +0200

    filter advertisements at main page

--- CoWorkClient/cowork-app/src/views/CoWorkHome.vue
+++ CoWorkClient/cowork-app/src/views/CoWorkHome.vue
@@ -11,9 +11,26 @@
         </div>
     </div>
 </section>
+<section class="text-center" style="background-color: gainsboro; padding: 10px; display: flex;justify-content: center;">
+    <div>
+        <div><b>Cena do: {{this.rangeValue}} rsd</b></div>
+        <div>
+            <span>{{min_price}} rsd</span>
+            <input type="range" :min="min_price" :max="max_price" v-model="this.rangeValue" step="1" />
+            <span>{{max_price}} rsd</span>
+        </div>
+    </div>
+    <div style="width: 20px; border-right: 1px solid gray;"></div>
+    <div style="width: 20px;"></div>
+    <div>
+        <div><b>Prikazi samo slobodne prostore?</b></div>
+        <div><input type="checkbox" v-model="onlyFree" style="display: block;margin: auto;height: 20px;width: 20px;" />
+        </div>
+    </div>
+</section>
 <div class="container">
     <div class="row row-cols-1 row-cols-md-4 g-8 py-4">
-        <div class="col py-2" v-for="ad in advertisements" :key="ad">
+        <div class="col py-2" v-for="ad in filter_advertisements" :key="ad">
             <div class="card" style="height: 100%;">
                 <img v-if="ad.image!==null" :src="ad.image" class="card-img-top" alt="...">
                 <div class="card-body">
@@ -21,7 +38,7 @@
                     <div class="card-text">
                         <div class="fw-bold"> <i class="bi bi-geo-alt-fill"></i> {{ ad.address }}</div>
                         <div> {{ ad.description }}</div>
-                        <div> Trenutno: {{ ad.isFree? 'slobodan' : 'zauzet' }}</div>
+                        <div :style="{'color': ad.isFree? 'green' : 'red'}"> Trenutno: {{ ad.isFree? 'slobodan' : 'zauzet' }}</div>
                         <div style="color: gray;"> <i class="bi bi-cash"></i> {{ ad.pricePerHour }} po satu</div>
                     </div>
                 </div>
@@ -41,12 +58,15 @@ export default {
     components: {
         MenuOptions
     },
+    data() {
+        return {
+            rangeValue: null,
+            onlyFree: false
+        }
+    },
     created() {
         this.$store.dispatch('getUser')
         this.$store.dispatch('getAllAd')
-    },
-    methods: {
-
     },
     computed: {
         user() {
@@ -54,7 +74,34 @@ export default {
         },
         advertisements() {
             return this.$store.state.advertisement.advertisements
+        },
+        min_price() {
+            return Math.min(...this.advertisements.map(ad => ad.pricePerHour))
+        },
+        max_price() {
+            return  Math.max(...this.advertisements.map(ad => ad.pricePerHour))
+        },
+        filter_advertisements() {
+            let ads= this.$store.state.advertisement.advertisements.filter(ad => this.onlyFree ? ad.isFree : true)
+            if(this.rangeValue !== null && this.rangeValue >= 0){
+                ads= ads.filter(ad => {
+                if (ad.pricePerHour <= this.rangeValue)
+                    return true
+                return false
+                })
+            }
+            return ads
         }
+    },
+    watch: {
+        max_price: {
+            handler: function (newV, oldV) {
+                if(oldV <=0 && newV >0 && this.rangeValue === null) {
+                   this.rangeValue = newV
+                }
+            },
+            deep: true
+        },
     }
 }
 </script>
